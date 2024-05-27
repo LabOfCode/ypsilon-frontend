85d41403103b53f8abafefd0201b8c82fb5502c4
Andrii Hutnyk <andreigutnik@gmail.com>
2024-05-27 22:07:53 +0200

    Update Container.ts

--- src/components/Container.ts
+++ src/components/Container.ts
@@ -1,10 +1,13 @@
 import styled from 'styled-components';
 
 const Container = styled.div`
-  max-width: 360px;
   padding: 0 16px;
   margin: 0 auto;
   outline: 1px solid red;
+
+  @media ${({ theme }) => theme.media.mobile} {
+    max-width: 360px;
+  }
 
   @media ${({ theme }) => theme.media.tablet} {
     width: 744px;
