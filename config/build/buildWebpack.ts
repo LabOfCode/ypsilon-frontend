579ea3fdd7d25a2fe5400373f39f676e80db851f
Andrii Hutnyk <andreigutnik@gmail.com>
2024-05-28 13:20:26 +0200

    Update buildWebpack.ts

--- config/build/buildWebpack.ts
+++ config/build/buildWebpack.ts
@@ -14,6 +14,7 @@ export function buildWebpack(options: BuildOptions): webpack.Configuration {
     mode: mode ?? 'development',
     entry: paths.entry,
     output: {
+      publicPath: '/ypsilon-frontend/',
       path: paths.output,
       filename: '[name].[contenthash].js',
       clean: true,
