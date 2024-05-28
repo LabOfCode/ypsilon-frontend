58470fccd0bfe554247553d3677d32b211b46ea2
Andrii Hutnyk <andreigutnik@gmail.com>
2024-05-28 13:55:44 +0200

    update config

--- webpack.config.ts
+++ webpack.config.ts
@@ -12,7 +12,7 @@ interface EnvVariables {
 export default (env: EnvVariables) => {
   const paths: BuildPaths = {
     entry: path.resolve(__dirname, 'src', 'index.tsx'),
-    output: path.resolve(__dirname, 'build'),
+    output: path.resolve(__dirname, 'dist'),
     html: path.resolve(__dirname, 'public', 'index.html'),
     public: path.resolve(__dirname, 'public'),
     src: path.resolve(__dirname, 'src'),
