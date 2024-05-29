4a2f3c1f2a018247072c59a295447a06beea4c9e
Andrii Hutnyk <andreigutnik@gmail.com>
2024-05-29 11:28:25 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -20,7 +20,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
       loading={null}
       persistor={persistor}
     > */}
-    <BrowserRouter basename={isDev ? '/' : '/ypsilon-frontend/'}>
+    <BrowserRouter basename={isDev ? '/' : '/ypsilon-frontend'}>
       <HelmetProvider>
         <ThemeProvider theme={theme}>
           <App />
