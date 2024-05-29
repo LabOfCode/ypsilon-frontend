f236e178b19f8dfb0690fb877d356d1fdb142d0c
Andrii Hutnyk <andreigutnik@gmail.com>
2024-05-29 11:14:34 +0200

    Add mapSection

---
+++ src/pages/VacanciesPage/VacanciesPage.tsx
@@ -0,0 +1,11 @@
+import { MapSection } from '@/components/MapSection/MapSection';
+
+const VacanciesPage = () => {
+  return (
+    <div>
+      <MapSection />
+    </div>
+  );
+};
+
+export default VacanciesPage;
