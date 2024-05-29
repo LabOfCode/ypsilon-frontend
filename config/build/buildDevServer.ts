621245e4f15df6ca61518dc1583f69f01c1472c8
Andrii Hutnyk <andreigutnik@gmail.com>
2024-05-29 22:33:38 +0200

    Update settings

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -3,20 +3,9 @@ import type { Configuration as DevServerConfiguration } from 'webpack-dev-server
 import { BuildOptions } from './types/types';
 
 export function buildDevServer(options: BuildOptions): DevServerConfiguration {
-  const isDev = options.mode === 'development';
-
   return {
-    static: {
-      directory: options.paths.output,
-    },
-    historyApiFallback: {
-      index: isDev ? '/index.html' : '/ypsilon-frontend/index.html',
-    },
     port: options.port ?? 3000,
     open: true,
     hot: true,
-    devMiddleware: {
-      publicPath: isDev ? '' : '/ypsilon-frontend/',
-    },
   };
 }
