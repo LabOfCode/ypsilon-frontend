13d39d5da9889532b93b61eb5e1b2b1a0a1eb399
InnaTereschenko <120989288+InnaTereschenko@users.noreply.github.com>
2024-06-12 20:03:40 +0300

    add hero content

--- src/components/HeroSection/HeroSection.styled.ts
+++ src/components/HeroSection/HeroSection.styled.ts
@@ -1,4 +1,5 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+
 import Flex from '../Flex/Flex';
 
 export const HeroWrapper = styled.div`
@@ -6,14 +7,97 @@ export const HeroWrapper = styled.div`
   height: 100vh;
   position: relative;
   overflow: hidden;
+  margin-bottom: 40px;
+`;
+
+export const SlideImage = styled.div<{ bgImage: string }>`
+  height: 100vh;
+  background-size: cover;
+  background-position: center;
+  background-image: url(${props => props.bgImage});
+  opasity: hsla(0, 0%, 0%, 0.25);
 `;
 
 export const HeroContent = styled.div`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  text-align: center;
-  color: white;
-  z-index: 2;
+position: absolute;
+top: 184px;
+left: 50%;
+transform: translateX(-50%);
+width: 328px;
+text-align: center;
+
+
+    @media ${({ theme }) => theme.media.tablet} {
+    width: 664px;
+    margin: 0 auto 30px auto;
+  }
+
+//   @media ${({ theme }) => theme.media.desktop} {
+//     width: 905px;
+//     margin: 0 auto 48px auto;
+  }
 `;
+
+export const HeroTitle = styled.h1`
+  color: ${({ theme }) => theme.colors.colorWhite};
+  text-align: left;
+  font-size: 24px;
+  font-weight: 700;
+  line-height: 35px;
+  margin-bottom: 8px;
+
+  @media ${({ theme }) => theme.media.tablet} {
+  width: 495px;
+    font-size: 36px;
+    line-height: 50.4px;
+    margin-bottom: 16px;
+  }
+
+  @media ${({ theme }) => theme.media.desktop} {
+  width: 787px;
+    font-size: 64px;
+    line-height: 89.6px;
+    margin-bottom: 40px;
+  }
+`;
+
+export const HeroText = styled.p`
+  color: ${({ theme }) => theme.colors.colorWhite};
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 17.07px;
+  text-align: left;
+  margin-bottom: 32px;
+
+  @media ${({ theme }) => theme.media.tablet} {
+  width: 495px;
+    font-size: 20px;
+    font-weight: 600;
+    line-height: 28px;
+    margin-bottom: 24px;
+  }
+
+  @media ${({ theme }) => theme.media.desktop} {
+  width: 787px;
+    font-size: 25px;
+    font-weight: 700;
+   
+  }
+`;
+
+
+
+export const FlexStyled = styled(Flex)`
+  gap: 15px;
+  flex-wrap: wrap;
+  justify-content: space-between;
+
+  @media ${({ theme }) => theme.media.tablet} {
+  flex-wrap: nowrap;
+    gap: 20px;
+  }
+
+//   @media ${({ theme }) => theme.media.desktop} {
+//     gap: 24px;
+//   }
+// `;
--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -1,11 +1,24 @@
-import { HeroWrapper, HeroContent } from "./HeroSection.styled";
 import Slider from 'react-slick';
-
-import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel/slick/slick.css';
+
+import slide1 from '@/assets/images/employment_0 2.jpg';
+import slide2 from '@/assets/images/employment_0-2 2.jpg';
+import slide3 from '@/assets/images/employment_1-2 2.jpg';
+import slide4 from '@/assets/images/employment_1-3.jpg';
+import slide5 from '@/assets/images/employment_2 2.jpg';
+import slide6 from '@/assets/images/employment_2-2 2.jpg';
+import slide7 from '@/assets/images/employment_2-3 11.jpg';
+import slide8 from '@/assets/images/employment_2-4 1.jpg';
+import slide9 from '@/assets/images/employment_3-2 1.jpg';
+
+import { HeroContent, HeroTitle, HeroWrapper, SlideImage, HeroText, FlexStyled} from './HeroSection.styled';
+import { Button } from '../Button/Button';
+import { LinkButton } from '../Button/Button';
+
 
 export const HeroSection = () => {
- const settings = {
+  const settings = {
     dots: false,
     infinite: true,
     speed: 500,
@@ -13,30 +26,53 @@ export const HeroSection = () => {
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 3000,
-     fade: true,
+    fade: true,
     cssEase: 'linear',
     arrows: false,
+    responsive: [
+      {
+        breakpoint: 743,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
+        },
+      },
+      {
+        breakpoint: 1440,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
+        },
+      },
+    ],
   };
 
-    return (
-        <HeroWrapper>
-     
- <Slider {...settings}>
-       <div>
-            <img src={require('../../assets/images/employment_0 2.jpg')} alt="Slide 1" style={{ width: '100vw', height: '100vh', objectFit: 'cover' }} />
-          </div>
-          <div>
-            <img src={require('../../assets/images/employment_0-2 2.jpg')} alt="Slide 2" style={{ width: '100vw', height: '100vh', objectFit: 'cover' }} />
-          </div>
-          <div>
-            <img src={require('../../assets/images/employment_1-2 2.jpg')} alt="Slide 3" style={{ width: '100vw', height: '100vh', objectFit: 'cover' }} />
-          </div>
+  return (
+    <HeroWrapper>
+      <Slider {...settings}>
+        <SlideImage bgImage={slide1} />
+        <SlideImage bgImage={slide2} />
+        <SlideImage bgImage={slide3} />
+        <SlideImage bgImage={slide4} />
+        <SlideImage bgImage={slide5} />
+        <SlideImage bgImage={slide6} />
+        <SlideImage bgImage={slide7} />
+        <SlideImage bgImage={slide8} />
+        <SlideImage bgImage={slide9} />
       </Slider>
-                <HeroContent>
-                
-                </HeroContent>        
-       
-       
-        </HeroWrapper>
-    )
-}
+      <HeroContent>
+        <HeroTitle>Агенція з працевлаштування в Чехії</HeroTitle>
+        <HeroText>Робота в Чехії. Ypsylon - компанія, яка надає повний супровід та підбір вакансій</HeroText>
+        <FlexStyled>
+<Button $teal to=''>Залишити заявку</Button>
+          <LinkButton $yellow to=''>Підібрати вакансію</LinkButton>
+         
+        </FlexStyled>
+      </HeroContent>
+    </HeroWrapper>
+  );
+};
