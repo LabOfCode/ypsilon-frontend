4b4eb48c7c71da720e7ce8014815b12ad09e32a1
Andrii Hutnyk <andreigutnik@gmail.com>
2024-08-18 18:22:03 +0200

    Add server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`client start port ${PORT}`);
+});
