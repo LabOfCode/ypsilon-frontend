85ff4d5836f0352893d29502cd99bb698131e91d
Andrii Hutnyk <andreigutnik@gmail.com>
2024-08-18 19:37:55 +0200

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -8,7 +8,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`client start port ${PORT}`);
 });
