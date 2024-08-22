dc0fa93849b45b3271885fb0755d711d8a59beae
Andrii Hutnyk <andreigutnik@gmail.com>
2024-08-22 14:57:54 +0200

    Update deploy to VPS server

--- public/server.mjs
+++ public/server.mjs
@@ -10,10 +10,10 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 // Использование __dirname для определения пути к статическим файлам
-app.use(express.static(path.join(__dirname, 'dist')));
+//app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
