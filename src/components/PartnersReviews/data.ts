5d449ffee8212f9e8074778a238155e0b3e1bf1e
Ribich1 <ribich@ukr.net>
2025-02-26 23:16:52 +0100

    fix translate

--- src/components/PartnersReviews/data.ts
+++ src/components/PartnersReviews/data.ts
@@ -13,23 +13,23 @@ export const data = [
     id: 1,
     count: 5,
     review:
-      'Lorem ipsum dolor sit amet consectetur. Tortor ullamcorper ornare dui laoreet. Felis dolor est in vestibulum eleifend enim. Velit auctor neque condimentum tempor ut turpis pretium in sit. Eu sagittis hendrerit sit mattis turpis.',
+      'S agenturou Ypsilon work jsme stále spolupracujeme od roku 2021, od té doby jsme rozjeli provoz od jednosměnného ke třísměnnému, a ten rozvoj nám pomohla vůči zaměstnancům zvládnout agentura Ypsilon work. Vyrábíme plastové plynové lahve, a jejich kompletace je pečlivá a odpovědná práce, proto pracovnici při nástupu testujeme, a musím říct, že i ve dnešní době   Agentura schopná dodat vyhovující kandidáty. Dokumentace spojena s přidělením pracovníků vždy včas a podle řádné legislativy. Takže, agenturu Ypsilon work určité doporučuji jak velmi schopného, spolehlivého, flexibilního a vstřícného partnera.',
     logo: logo1,
     company: 'HPC Research',
   },
   {
     id: 2,
-    count: 4,
+    count: 5,
     review:
-      'Lorem ipsum dolor sit amet consectetur. At vestibulum elementum eget varius faucibus ut nisl. Felis aenean elementum in felis risus purus. Donec condimentum tincidunt a consectetur habitasse odio. Cursus potenti nec libero.',
+      'S agenturou práce YPSILON WORK s.r.o. jsme začali spolupracovat již před několika lety, v době, kdy byl trh práce velmi složitý a my jsme nebyli schopni vlastními silami pokrýt potřebu výroby lidskými zdroji.Za dobu naší spolupráce hodnotíme velmi vysoko úroveň komunikace, odpovědnost a úsilí agentury vyjít vstříc našim potřebám a požadavkům pokrýt potřeby výroby ve velmi těžkém pracovním prostředí. Primárně u této agentury poptáváme dělnické profese do těžkého slévárenského provozu a to jak kvalifikované (obráběči, kováři, taviči atd.), tak i méně kvalifikované (brusiči). Není úplně jednoduché takovéto pozice obsadit a oceňujeme, že se toho agentura nezalekla.Pevně doufáme, že spolupráce s agenturou bude bezproblémová i v následujících letech.',
     logo: logo2,
     company: 'Tatra, a.s.',
   },
   {
     id: 3,
-    count: 2,
+    count: 5,
     review:
-      'Lorem ipsum dolor sit amet consectetur. Gravida gravida tellus tellus gravida sed at pellentesque. Velit tristique morbi massa dictum eu nunc sed. Pharetra sapien pharetra leo quam nunc cursus. Felis dui est suspendisse curabitur.',
+      'Pozitivní praxe spolupráce mezi obchodní partneři Ypsilon work s.r.o. a KUVAG CR, spol. s.r.o. v oblasti zprostředkovaní zaměstnaní',
     logo: logo3,
     company: 'Kuvag',
   },
@@ -37,7 +37,7 @@ export const data = [
     id: 4,
     count: 5,
     review:
-      'Lorem ipsum dolor sit amet consectetur. Gravida gravida tellus tellus gravida sed at pellentesque. Velit tristique morbi massa dictum eu nunc sed. Pharetra sapien pharetra leo quam nunc cursus. Felis dui est suspendisse curabitur.',
+      'S Agenturou Ypsilon work s.r.o. jsme začali spolupracovat v roce 2022 na projektu National Pen. Oceňuji zejména profesionalitu a splnění všech podmínek a dodání pracovníků včetně dokumentace. Velice si cením rychlosti, flexibility a vstřícné domluvy a zejména kvality dodávaných pracovníků, kterými jsme mohli obsadit jak pozice na ručním oddělení, tak kvalifikované pozice ve skladu. Spolupráci s agenturou Ypsilon work s.r.o. hodnotím velmi pozitivně. Jako velmi schopného, spolehlivého a flexibilního partnera ji ráda doporučím.',
     logo: logo4,
     company: 'Pens - Personalized promotion product',
   },
@@ -45,7 +45,7 @@ export const data = [
     id: 5,
     count: 5,
     review:
-      'Lorem ipsum dolor sit amet consectetur. Gravida gravida tellus tellus gravida sed at pellentesque. Velit tristique morbi massa dictum eu nunc sed. Pharetra sapien pharetra leo quam nunc cursus. Felis dui est suspendisse curabitur.',
+      'Pozitivní praxe spolupráce mezi obchodní partneři Ypsilon work s.r.o. a Adler Pelzer Automotive GmbH v oblasti zprostředkovaní zaměstnaní',
     logo: logo5,
     company: 'Adler Pelzer Automotive GmbH',
   },
@@ -53,31 +53,28 @@ export const data = [
     id: 6,
     count: 5,
     review:
-      'Lorem ipsum dolor sit amet consectetur. Gravida gravida tellus tellus gravida sed at pellentesque. Velit tristique morbi massa dictum eu nunc sed. Pharetra sapien pharetra leo quam nunc cursus. Felis dui est suspendisse curabitur.',
+      'Splněni zakázky firmy KS Lettershop Logistics Services dodaní operátoru balení obleku a kontrola kvality vracených věcí.',
     logo: logo6,
     company: 'KS Lettershop Logistics Services',
   },
   {
     id: 7,
     count: 5,
-    review:
-      'Lorem ipsum dolor sit amet consectetur. Gravida gravida tellus tellus gravida sed at pellentesque. Velit tristique morbi massa dictum eu nunc sed. Pharetra sapien pharetra leo quam nunc cursus. Felis dui est suspendisse curabitur.',
+    review: 'Nábor zámečniku pro firmu WEILER Holoubkov s.r.o - výroba obráběcích strojů',
     logo: logo7,
     company: 'WEILER Holoubkov s.r.o.',
   },
   {
     id: 8,
     count: 5,
-    review:
-      'Lorem ipsum dolor sit amet consectetur. Gravida gravida tellus tellus gravida sed at pellentesque. Velit tristique morbi massa dictum eu nunc sed. Pharetra sapien pharetra leo quam nunc cursus. Felis dui est suspendisse curabitur.',
+    review: 'Nábor topenařu, izolatoru, natěrače a svareče pro firmu Systherm s.r.o.',
     logo: logo8,
     company: 'The SYSTHERM company',
   },
   {
     id: 9,
     count: 5,
-    review:
-      'Lorem ipsum dolor sit amet consectetur. Gravida gravida tellus tellus gravida sed at pellentesque. Velit tristique morbi massa dictum eu nunc sed. Pharetra sapien pharetra leo quam nunc cursus. Felis dui est suspendisse curabitur.',
+    review: 'Poskytnuti operátoru výroby pro Ašskou galvanovnu, včetně ubytovaní a dojezdu.  ',
     logo: logo9,
     company: 'Ašská galvanovna s.r.o.',
   },
